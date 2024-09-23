31a82806f7eb0d659ec391d06f25c740e1f27a5d
SACHIN KUMAR RAJBHER <86919694+sachinrajbher15@users.noreply.github.com>
2024-09-23 19:23:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	const freq = {};
+	for(let char of str){
+		if(freq[char]){
+			freq[char] +=1;
+		}
+		else{
+			freq[char] = 1;
+		}
+	}
+	for(let char of str){
+		if(freq[char] === 1){
+			return char;
+		}
+	}
+	return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
